d22d7fb8a263479cf2c1b6ed2c0b9551c86e21ae
Burner Board <burner@rmc.local>
2013-08-24 13:45:46 -0700

    Version 2.0 of the Cape patterns.
    Note that this has a low brightness setting for use from a laptop.
    Remove the setting to get full brightness when on battery (vs. USB port).

--- Cape/Cape.ino
+++ Cape/Cape.ino
@@ -2,11 +2,11 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-// Take a strip of LEDS and have the lights flash moving from the two ends of the strip 
-// into the middle. 
-// 
+// Have fun with a strip of LEDs!
+//
 // Derived from the FastSPI_LED2 FirstLight example
 //
+// Blame Steve Herrod as needed.
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 // How many leds are in the strip?
@@ -18,13 +18,41 @@
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 //#define CLOCK_PIN 8
 
+// Simplify dealings with the colors
+#define WHITE CRGB::White
+#define BLACK CRGB::Black
+#define BLUE  CRGB::Blue
+#define GREEN CRGB::Green
+#define YELLOW CRGB::Yellow
+#define PINK   CRGB::Pink
+#define RED    CRGB::Red
+#define ORANGE CRGB::Orange
+#define PURPLE CRGB::Purple
+#define INDIGO CRGB::Indigo
+#define MAX_COLORS 10
+
+CRGB colorArray[MAX_COLORS] = {WHITE, BLACK, BLUE, GREEN, YELLOW, PINK, RED, ORANGE, PURPLE, INDIGO};
+
+
+// We'll use these for random color patterns
+#define RAND8_SEED  111
+byte rand8seed = RAND8_SEED;
+
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
+// Global used to choose when to rotate through patterns
+int loopCount;
+
+//
 // This function sets up the ledsand tells the controller about them
+//
 void setup() {
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
+  
+  loopCount = 0;
+  
+  // sanity check delay - allows reprogramming if accidently blowing power w/leds
+  delay(2000);
 
       // Uncomment one of the following lines for your leds arrangement.
       // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
@@ -34,7 +62,6 @@ void setup() {
       // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-
       FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
 
       // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
@@ -42,32 +69,116 @@ void setup() {
       // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      
+  // Temporarily ratchet down so it doesn't kill the USB port
+  LEDS.setBrightness(64);
+
+}
+
+//
+// Move a single led from LED 0 to the halfway point. Simultaneously 
+// move a led from LED NUM_LEDS backwards to the halfway point.
+//
+int SymmetricWavePattern(CRGB color) {
+  
+   for(int led = 1; led < (NUM_LEDS/2); led++) {
+
+      leds[led] = color;
+      leds[(NUM_LEDS-led)] = color;
+
+      // Show the leds (two of which are lit)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(100);
+
+      // Turn the leds back to black for the next loop around
+      leds[led] = BLACK;
+      leds[NUM_LEDS-led] = BLACK;
+   }
+   return 0;
+}
+
+//
+// Rainbow time
+//
+int RainbowPattern() {
+   
+  SymmetricWavePattern(RED);
+  SymmetricWavePattern(ORANGE);
+  SymmetricWavePattern(YELLOW);
+  SymmetricWavePattern(GREEN);
+  SymmetricWavePattern(BLUE);
+  SymmetricWavePattern(PURPLE);
+  SymmetricWavePattern(PURPLE);
+  
+}
+
+//
+// Honor 'merica
+//
+int PatriotPattern() {
+  
+  SymmetricWavePattern(RED);
+  SymmetricWavePattern(WHITE);
+  SymmetricWavePattern(BLUE);
+  
+  // Now do a single run down the lights going patriot mode
+  for(int led = 1; led < NUM_LEDS; led++) {
+      switch(led%3) {
+        case 1: leds[led] = RED; break;
+        case 2: leds[led] = WHITE; break;
+        case 0: leds[led] = BLUE; break;
+      }
+      FastLED.show();
+      delay(100);
+      leds[led] = BLACK;
+    }
+
+  // Leave a red-white-blue pattern on as it snakes down the line    
+  for(int led = 1; led < NUM_LEDS; led++) {
+      switch(led%3) {
+        case 1: leds[led] = RED; break;
+        case 2: leds[led] = WHITE; break;
+        case 0: leds[led] = BLUE; break;
+      }
+      if (led > 3) {
+        leds[led-2] = BLACK;
+      }
+      delay(500);
+      FastLED.show();
+    }
+}
+
+//
+// Turn the whole string on with each one being a random color
+//
+int AllOnRandomPattern() {
+  
+  for (int i=1; i<NUM_LEDS; i++) {
+    byte rand = random8() % MAX_COLORS;
+    
+    leds[i] = colorArray[rand];
+    FastLED.show();
+    delay(100);
+  }  
 }
 
 // This function runs over and over, and is where you do the magic to light
 // your leds.
 
 void loop() {
-
-   // Move a single white led from LED 0 to the halfway point. Simultaneously 
-   // move a white led from LED NUM_LEDS backwards to the halfway point.
-
-   for(int whiteLed = 1; whiteLed < (NUM_LEDS/2); whiteLed = whiteLed + 1) {
-
-      // Turn our current led on to white, then show the leds
-      leds[whiteLed] = CRGB::Blue;
-
-      // Do the same thing counting backwards from the end
-      leds[(NUM_LEDS-whiteLed)] = CRGB::Blue;
-
-      // Show the leds (two of which are lit)
-      FastLED.show();
-
-      // Wait a little bit
-      delay(500);
-
-      // Turn the leds back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
-      leds[NUM_LEDS-whiteLed] = CRGB::Black;
-   }
+  
+  loopCount++;
+  SymmetricWavePattern(WHITE);
+  
+  if ((loopCount > 100)) {
+    PatriotPattern(); 
+    SymmetricWavePattern(BLACK);
+    AllOnRandomPattern();
+    SymmetricWavePattern(BLACK);
+    RainbowPattern();
+    loopCount = 0;
+  }  
+ 
 }
